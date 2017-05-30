6a3803d7e819b5c3ae3eafd2f48d66023a6c8be3
bjornus <dmitro.szewczuk@wp.pl>
2017-05-30 22:58:35 +0200

    File customizing to pylinter

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 import glob
 import os
 
 # Getting path of all files in directory
-dir_path = os.path.dirname(os.path.relpath(__file__))
+DIR_PATH = os.path.dirname(os.path.relpath(__file__))
 
-relative_path = input("Give relative path of header files location (e.g headers/): ")
-extension = input("Give extension of header files (e.g .h / .hpp): ")
-output_path = input("Give path to catalog in witch create HTML documentation (e.g output/): ")
+RELATIVE_PATH = input("Give relative path of header files location (e.g headers/): ")
+EXTENSION = input("Give extension of header files (e.g .h / .hpp): ")
+OUTPUT_PATH = input("Give path to catalog in witch create HTML documentation (e.g output/): ")
 
-if relative_path[-1:] != "/":
-    relative_path += "/"
+if RELATIVE_PATH[-1:] != "/":
+    RELATIVE_PATH += "/"
 
-if output_path[-1:] != "/":
-    output_path += "/"
+if OUTPUT_PATH[-1:] != "/":
+    OUTPUT_PATH += "/"
 
-if extension[:0] != ".":
-    extension = "." + extension
+if EXTENSION[:0] != ".":
+    EXTENSION = "." + EXTENSION
 
-header_glob = glob.glob(dir_path + relative_path + "*" + extension)
+HEADER_GLOB = glob.glob(DIR_PATH + RELATIVE_PATH + "*" + EXTENSION)
 
-if len(header_glob) < 1:
+if len(HEADER_GLOB) < 1:
     print("Error! No files to documentation")
     exit(1)
 
 objects = []
 
 # Printing content of files
-for file in header_glob:
+for file in HEADER_GLOB:
     f = open(file, 'r').read().splitlines()
     for line in f:
         line = line.strip()
@@ -35,12 +35,12 @@ for file in header_glob:
             operator = line[2:7]
             content = line[8:len(line) - 1]
             if operator == "[nam]":
-                objects.append({"name": "", "description": "", "arguments": "", "returns": "", "class": ""})
+                objects.append({"name": "", "desc": "", "args": "", "returns": "", "class": ""})
                 objects[len(objects) - 1]["name"] = content
             if operator == "[des]":
-                objects[len(objects) - 1]["description"] = content
+                objects[len(objects) - 1]["desc"] = content
             if operator == "[arg]":
-                objects[len(objects) - 1]["arguments"] = content
+                objects[len(objects) - 1]["args"] = content
             if operator == "[ret]":
                 objects[len(objects) - 1]["returns"] = content
             if operator == "[cla]":
@@ -58,14 +58,14 @@ css_util_content = css_util.read()
 print("Reading templates completed.")
 
 # Saving final files
-save_file = open(output_path + "index.html", "w")
+save_file = open(OUTPUT_PATH + "index.html", "w")
 save_content = ""
 
 for o in objects:
     save_content += "<p>\nName: " + o["name"] + "<br>\n"
     save_content += "Class: " + o["class"] + "<br>\n"
-    save_content += "Description: " + o["description"] + "<br>\n"
-    save_content += "Arguments: " + o["arguments"] + "<br>\n"
+    save_content += "Description: " + o["desc"] + "<br>\n"
+    save_content += "Arguments: " + o["args"] + "<br>\n"
     save_content += "Returning: " + o["returns"] + "<br>\n</p>\n"
     save_content += "<hr>"
 
@@ -75,7 +75,10 @@ after_html_file = "</div>" \
 
 save_file.write(html_util_content + save_content + after_html_file)
 
-css_file = open(output_path + "styles.css", "w")
+css_file = open(OUTPUT_PATH + "styles.css", "w")
 css_file.write(css_util_content)
 
-print("Writing html file completed.")
+print('Writing html file completed.')
+
+css_file.close()
+html_util.close()
