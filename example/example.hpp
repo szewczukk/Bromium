0c2848e8dc93797cc8efe6fa1252e737da7b8c8a
bjornus <dmitro.szewczuk@wp.pl>
2017-06-05 19:52:01 +0200

    Example output added

--- example/example.hpp
+++ example/example.hpp
@@ -19,24 +19,3 @@ class foobar
         return bar + bar2;
     }
 }
-class barfoo
-{
-    //[nam][isEqual]
-    //[cla][barfoo]
-    //[des][Returns true if first bar is qual to second bar]
-    //[arg][bar - first bar ; bar2 - second bar]
-    //[ret][bool when first bar is equal to second]
-    bool isEqual(int bar, int bar2)
-    {
-        return bar == bar2 ? true : false;
-    }
-    //[nam][sum]
-    //[cla][barfoo]
-    //[des][Returns sum of bar and bar2]
-    //[arg][bar - first bar ; bar2 - second bar]
-    //[ret][sum of both bars in integer type]
-    int sum(int bar, int bar2)
-    {
-        return bar + bar2;
-    }
-}
